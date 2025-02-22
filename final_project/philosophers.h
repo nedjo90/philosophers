448fdaadebecdf08a0f08e309d41a5636619dfa6
Necati Han <nhan@k0r3p14.42mulhouse.fr>
2025-02-22 16:34:10 +0100

    check at beginning

--- final_project/philosophers.h
+++ final_project/philosophers.h
@@ -6,17 +6,39 @@
 /*   By: nhan <nhan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 14:57:42 by nhan              #+#    #+#             */
-/*   Updated: 2025/02/22 15:11:12 by nhan             ###   ########.fr       */
+/*   Updated: 2025/02/22 15:55:30 by nhan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
+# define FALSE 0
+# define TRUE 1
+# define ERROR_NB_INPUT "Please enter the right number of input"
+# define ERROR_NOT_DIGIT "Please enter only digit"
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_atoi(const char *str);
+typedef struct s_input
+{
+	int		nb_philo;
+	int		time_die;
+	int		time_eat;
+	int		time_sleep;
+	int		nb_time_eat;
+}			t_input;
+
+typedef struct s_philo
+{
+	int		id;
+	t_input	input;
+}			t_philo;
+
+int			ft_atoi(const char *str);
+int			ft_isdigit(int c);
+size_t		ft_strlen(const char *s);
 
 #endif
--- final_project/pĥilosophers.c
+++ final_project/pĥilosophers.c
@@ -6,14 +6,72 @@
 /*   By: nhan <nhan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:00:24 by nhan              #+#    #+#             */
-/*   Updated: 2025/02/22 15:06:23 by nhan             ###   ########.fr       */
+/*   Updated: 2025/02/22 16:33:03 by nhan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	main(void)
+static int	ft_is_positive_number(char *arg, int *nb)
 {
-	printf("hello world\n");
-	return (0);
+	int	i;
+
+	i = 0;
+	*nb = -1;
+	while (arg[i])
+	{
+		if (!ft_isdigit(arg[i]))
+		{
+			printf("%s\n", ERROR_NOT_DIGIT);
+			return (FALSE);
+		}
+		i++;
+	}
+	*nb = ft_atoi(arg);
+	return (TRUE);
+}
+
+static t_input	ft_check_input(int argc, char **argv)
+{
+	t_input	input;
+
+	if (!(argc == 5 || argc == 6))
+		printf("%s\n", ERROR_NB_INPUT);
+	ft_is_positive_number(argv[1], &(input.nb_philo));
+	ft_is_positive_number(argv[2], &(input.time_die));
+	ft_is_positive_number(argv[3], &(input.time_eat));
+	ft_is_positive_number(argv[4], &(input.time_sleep));
+	if (argc == 5)
+		input.nb_time_eat = -1;
+	else
+		ft_is_positive_number(argv[5], &input.nb_time_eat);
+	return (input);
+}
+
+static void	ft_display_input(t_input input)
+{
+	printf("Number of philosophers: %d\n", input.nb_philo);
+	printf("Time before a philosopher dies without eating: %d\n",
+		input.time_die);
+	printf("Duration of a meal for a philosopher: %d\n", input.time_eat);
+	printf("Duration of sleep for a philosopher: %d\n", input.time_sleep);
+	if (input.nb_time_eat == -1)
+		printf("No eating limit\n");
+	else
+		printf("Number of times each philosopher must eat: %d\n",
+			input.nb_time_eat);
+}
+
+int	main(int argc, char **argv)
+{
+	t_input input;
+
+	input = ft_check_input(argc, argv);
+	if (!(input.nb_philo < 0 && input.time_die < 0 && input.time_eat < 0
+			&& input.time_sleep < 0 && input.nb_time_eat < 0))
+	{
+		ft_display_input(input);
+		return (0);
+	}
+	return (1);
 }
