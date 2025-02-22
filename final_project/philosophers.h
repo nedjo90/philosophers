2803017ef3eda6f90b07ca7aa93ef26c8fcb5c41
Necati Han <nhan@k0r3p14.42mulhouse.fr>
2025-02-22 15:14:18 +0100

    update

---
+++ final_project/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhan <nhan@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 14:57:42 by nhan              #+#    #+#             */
+/*   Updated: 2025/02/22 15:11:12 by nhan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_atoi(const char *str);
+
+#endif
---
+++ learning/Learning.md
@@ -0,0 +1,8 @@
+to do:
+	- check if 4 o 5 arguments otherwise => error
+	- arguments :
+		1. the number of philosophers
+		2. the time a philosopher will die if he doesn't eat
+		3. the time it takes a philosopher to eat
+		4. the time it takes a philosopher to sleep
+		5. (OPTIONAL) Number of times all the philosophers need to eat before terminating the program
